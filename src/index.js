b329e17bbb453ca3bbfada4892b168db283ce4b4
mudassiralam7 <mudassiralam7@gmail.com>
2025-04-28 09:57:25 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom";   // ✅ only 'react-dom' not 'react-dom/client'
 import App from "./components/App";
 
-
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
